4cfce9693d07a31609a0f3dea1872a6818add3b2
dwblmo <dwblmo@sourceforge.net>
2016-03-21 20:48:57 +0100

    delete of wrong link
    delete of wrong link

--- PHP-Fusion6/trainer.php
+++ PHP-Fusion6/trainer.php
@@ -7,7 +7,6 @@ echo '<h1>Trainer: '.$dh_anker.'</h1>';
 
 echo 'Das angeklickte Team wird von '.$dh_anker.' trainiert.';
 
-echo '<br><br>Informationen zu '.$dh_anker.' findest Du im '.dh_link('http://epl.ep.funpic.de/include.php?path=login/userinfo.php&name='.$dh_anker,'Benutzerprofil');
 echo '</div>';
 
 ?>
